2a35f3f98e0407021fbb63eff2d218b685f749ee
bad <badatnames@tutanota.com>
2022-01-02 19:55:17 +0100

    Fix unused var warning

--- Maps/Level.gd
+++ Maps/Level.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var lose_scn = preload("res://UI/Lose.tscn")
 
-func on_Player_touched_ball(Ball):
+func on_Player_touched_ball(_ball: Ball):
 	var lose: Control = lose_scn.instance()
 	lose.rect_size = get_viewport_rect().size
 	add_child(lose)
