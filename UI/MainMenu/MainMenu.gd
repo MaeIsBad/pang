c4b6dfba451ef1d019e1a36bc07cd66734221959
bad <badatnames@tutanota.com>
2022-02-08 19:22:19 +0100

    Improve the directory structure

--- UI/MainMenu/MainMenu.gd
+++ UI/MainMenu/MainMenu.gd
@@ -7,7 +7,7 @@ func on_start_button_pressed():
 	$MainMenuCamera.center_on($LevelSelect.rect_position)
 
 func on_start_level_button_pressed():
-	get_tree().change_scene("res://UI/Main.tscn")
+	get_tree().change_scene("res://UI/InGameUI/InGameUI.tscn")
 
 func on_back_button_pressed():
 	$MainMenuCamera.center_on($Main.rect_position)
