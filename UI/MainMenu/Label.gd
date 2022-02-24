579d13a44bd5fbb1852b33d7b24e10fe6c15b26b
bad <badatnames@tutanota.com>
2022-02-24 08:15:41 +0100

    Import splashes from discord, select a random splash
    Adds a splashes.txt file in the root directory of the project
    and selects a random splash from it nevery time the main menu is opened

--- UI/MainMenu/Label.gd
+++
@@ -1,8 +0,0 @@
-extends Label
-
-export(Array, String) var splashes =  []
-
-func _ready():
-	var rand_index:int = randi() % splashes.size()
-	text = splashes[rand_index]
-
---
+++ UI/MainMenu/Splashes.gd
@@ -0,0 +1,19 @@
+extends Label
+
+var splashes = []
+
+func load_splashes():
+	var splash_file := File.new()
+	assert(splash_file.open("res://splashes.txt", File.READ) == OK)
+	while !splash_file.eof_reached():
+		var line := splash_file.get_line()
+		splashes.push_back(line)
+func get_random_splash():
+	return splashes[randi() % splashes.size()]
+
+func display_random_splash():
+	self.text = get_random_splash()
+
+func _ready():
+	load_splashes()
+	display_random_splash()
