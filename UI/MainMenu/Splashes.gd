8600f4ba9270fe3cfc498018871037a6ac88c461
bad <badatnames@tutanota.com>
2022-02-25 21:18:02 +0100

    Fix splashes sometimes not displaying

--- UI/MainMenu/Splashes.gd
+++ UI/MainMenu/Splashes.gd
@@ -1,14 +1,17 @@
 extends Label
 
-var splashes = []
+var splashes = load_splashes()
 
-func load_splashes():
+func load_splashes() -> Array:
+	var splashes = []
 	var splash_file := File.new()
 	assert(splash_file.open("res://splashes.txt", File.READ) == OK)
 	while !splash_file.eof_reached():
 		var line := splash_file.get_line()
 		splashes.push_back(line)
-
+	# Remove an empty splash at the end
+	splashes.pop_back()
+	return splashes
 func get_random_splash():
 	return splashes[randi() % splashes.size()]
 
