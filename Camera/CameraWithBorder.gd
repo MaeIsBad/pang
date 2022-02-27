ddf3e58045a6a6d2ffd79b478a064930f9838b4f
bad <badatnames@tutanota.com>
2022-02-28 00:07:48 +0100

    Cleanup: remove unused files

--- Camera/CameraWithBorder.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-func _ready():
-	assert(get_viewport().connect("size_changed", self,"update_border") == OK)
-	update_border()
-func update_border():
-	$StaticBody2D.set_rect(get_viewport_rect())
--- Maps/Walls.gd
+++
@@ -1,4 +0,0 @@
-extends StaticBody2D
-func _draw():
-	pass
-#	draw_rect(get_viewport_rect(),Color.white,false, 10.0)
