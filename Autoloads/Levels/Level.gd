819e575c9058e81f80f06f03b3a9839ef033a529
bad <badatnames@tutanota.com>
2022-02-28 20:44:23 +0100

    Add level ids

--- Autoloads/Levels/Level.gd
+++ Autoloads/Levels/Level.gd
@@ -12,9 +12,10 @@ var map_scene: String
 var difficulty: int = Difficulty.Easy
 var lives = 4
 var unlocked = true
+var id: String
 
-
-func _init(name_: String, map_scene_: String, difficulty_: int, lives_: int):
+func _init(id_: String, name_: String, map_scene_: String, difficulty_: int, lives_: int):
+	self.id = id_
 	self.name = name_
 	self.map_scene = map_scene_
 	self.difficulty = difficulty_
--- Autoloads/Levels/Levels.gd
+++ Autoloads/Levels/Levels.gd
@@ -1,12 +1,12 @@
-extends Node
+lextends Node
 
 var allLevels = [
-	Level.new("Test level 1", "res://Maps/TestMap.tscn", Level.Difficulty.Easy,6),
-	Level.new("Test level 2", "res://Maps/TestMap2.tscn", Level.Difficulty.Easy,5),
-	Level.new("Test level 3", "res://Maps/TestMap3.tscn", Level.Difficulty.Normal,4),
-	Level.new("Test level 4", "res://Maps/TestMap4.tscn", Level.Difficulty.Normal,3),
-	Level.new("Test level 5", "res://Maps/TestMap5.tscn", Level.Difficulty.Hard,2),
-	Level.new("Test level 6", "res://Maps/TestMap6.tscn", Level.Difficulty.Hard,1)
+	Level.new("test.1","Test level 1", "res://Maps/TestMap.tscn", Level.Difficulty.Easy,6),
+	Level.new("test.2","Test level 2", "res://Maps/TestMap2.tscn", Level.Difficulty.Easy,5),
+	Level.new("test.3","Test level 3", "res://Maps/TestMap3.tscn", Level.Difficulty.Normal,4),
+	Level.new("test.4","Test level 4", "res://Maps/TestMap4.tscn", Level.Difficulty.Normal,3),
+	Level.new("test.5","Test level 5", "res://Maps/TestMap5.tscn", Level.Difficulty.Hard,2),
+	Level.new("test.6","Test level 6", "res://Maps/TestMap6.tscn", Level.Difficulty.Hard,1)
 ]
 
 var levelsByDifficulty = {
@@ -15,10 +15,12 @@ var levelsByDifficulty = {
 	Level.Difficulty.Hard: []
 }
 
+var levelsById = {}
+
 func update_levels_globals():
 	for level in allLevels:
 		levelsByDifficulty[level.difficulty].append(level)
-
+		levelsById[level.id] = level
 func _ready():
 	update_levels_globals()
 	
