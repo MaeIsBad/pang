646084b956484d451a01d572533f63c6faa93826
Kacper Ślusarczyk <kacperuczyk@gmail.com>
2022-03-03 20:45:34 +0100

    Rename maps

--- Autoloads/Levels/Levels.gd
+++ Autoloads/Levels/Levels.gd
@@ -1,12 +1,12 @@
 extends Node
 
 var allLevels = [
-	Level.new("test.1","Test level 1", "res://Maps/TestMap.tscn", Level.Difficulty.Easy,6),
-	Level.new("test.2","Test level 2", "res://Maps/TestMap2.tscn", Level.Difficulty.Easy,5),
-	Level.new("test.3","Test level 3", "res://Maps/TestMap3.tscn", Level.Difficulty.Normal,4),
-	Level.new("test.4","Test level 4", "res://Maps/TestMap4.tscn", Level.Difficulty.Normal,3),
-	Level.new("test.5","Test level 5", "res://Maps/TestMap5.tscn", Level.Difficulty.Hard,2),
-	Level.new("test.6","Test level 6", "res://Maps/TestMap6.tscn", Level.Difficulty.Hard,1)
+	Level.new("test.1","Test level 1", "res://Maps/Map1.tscn", Level.Difficulty.Easy,6),
+	Level.new("test.2","Test level 2", "res://Maps/Map2.tscn", Level.Difficulty.Easy,5),
+	Level.new("test.3","Test level 3", "res://Maps/Map3.tscn", Level.Difficulty.Normal,4),
+	Level.new("test.4","Test level 4", "res://Maps/Map4.tscn", Level.Difficulty.Normal,3),
+	Level.new("test.5","Test level 5", "res://Maps/Map5.tscn", Level.Difficulty.Hard,2),
+	Level.new("test.6","Test level 6", "res://Maps/Map6.tscn", Level.Difficulty.Hard,1)
 ]
 
 var levelsByDifficulty = {
