0ff8c29b8af56f8b13ff390812ec9856bdbb1e20
bad <badatnames@tutanota.com>
2022-03-03 13:54:15 +0100

    Fix rpg not being able to break glass

--- Player/Weapons/RPG/RPGBullet.gd
+++ Player/Weapons/RPG/RPGBullet.gd
@@ -5,15 +5,18 @@ var explosion_script = preload("res://Player/Weapons/RPG/RPGExplosion.gd")
 export(float,0,1000) var UNITS_PER_SECOND = 800.0
 export var explosion_radius := 50.0
 
+const MAX_EXPLOSTION_TIME = 0.2
+
 func explode():
 	# Simulate explosion spreading to achieve a more satisfying
 	#; "pop" sound effect when the balls get destroyed
-	for b in $ExplosionArea.get_overlapping_bodies():
-		var body := b as CollisionObject2D
+	for body in $ExplosionArea.get_overlapping_bodies():
 		if body.has_method("touched_bullet"):
 			var distance := global_position.distance_to(body.global_position)
-			assert(get_tree().create_timer(distance/200.0).connect("timeout",body, "touched_bullet",[self]) == OK)
-	
+			if distance/200.0 < MAX_EXPLOSTION_TIME:
+				assert(get_tree().create_timer(distance/200.0).connect("timeout",body, "touched_bullet",[self]) == OK)
+			else:
+				body.touched_bullet(self)
 	var explosion = explosion_script.new(explosion_radius)
 	explosion.global_position = global_position
 	get_parent().add_child(explosion)
