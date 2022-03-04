61a16f65ff0220381553fc5643e4cd14a3ef707e
Kacper Ślusarczyk <kacperuczyk@gmail.com>
2022-03-04 16:29:29 +0100

    Redesign ingameui

--- UI/InGameUI/InGameUI.gd
+++ UI/InGameUI/InGameUI.gd
@@ -4,9 +4,13 @@ onready var lives_counter: Label = $ColorRect/MarginContainer/HBoxContainer/Info
 onready var score_display: Label = $ColorRect/MarginContainer/HBoxContainer/Score/Score
 onready var viewport: Viewport = $Main/ViewportContainer/Viewport
 onready var glitch_texture: TextureRect = $Main/GlitchContainer/GlitchTexture
-onready var weapon_texture: TextureRect = $ColorRect/MarginContainer/HBoxContainer/WeaponCharge/Weapon
-onready var weapon_charge: ProgressBar = $ColorRect/MarginContainer/HBoxContainer/WeaponCharge/WeaponCharge
+onready var weapon_texture: TextureRect = $ColorRect/MarginContainer/HBoxContainer/Info/WeaponCharge/Weapon
+onready var weapon_charge: ProgressBar = $ColorRect/MarginContainer/HBoxContainer/Info/WeaponCharge/WeaponCharge
 
+
+func set_weapon_scale():
+	weapon_texture.rect_scale = Vector2(5.0, 5.0)
+	weapon_texture.rect_rotation = -25.0
 
 func set_lives_counter(lives: int):
 	lives_counter.text = String(lives)
@@ -30,5 +34,6 @@ func glitch():
 	glitch_texture.material.set_shader_param("fade",0.01)
 	yield(get_tree().create_timer(0.1),"timeout")
 	glitch_texture.material.set_shader_param("fade",0.00)	
+	set_weapon_scale()
 
 
