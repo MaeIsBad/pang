39fd49b2d11ab61ccd5b93e59133ec8034dfe406
bad <badatnames@tutanota.com>
2022-03-04 23:25:16 +0100

    Nerf the default gun

--- Player/Weapons/Gun/Gun.gd
+++ Player/Weapons/Gun/Gun.gd
@@ -3,4 +3,4 @@ extends BaseWeapon
 func _init():
 	weapon_sprite = preload("res://Assets/Player/Weapons/weapon_1.png")
 	bullet_scn = preload("res://Player/Weapons/Gun/Bullet.tscn")
-	firing_rate = 0.1 
+	firing_rate = 0.3
