95e3d5dbb33a6d2f05a3b2ddaf03315089a283c7
bad <badatnames@tutanota.com>
2022-03-05 19:41:01 +0100

    Don't set the weapon texture in glitch, pls

--- UI/InGameUI/InGameUI.gd
+++ UI/InGameUI/InGameUI.gd
@@ -7,11 +7,6 @@ onready var glitch_texture: TextureRect = $Main/GlitchContainer/GlitchTexture
 onready var weapon_texture: TextureRect = $ColorRect/MarginContainer/HBoxContainer/Info/WeaponCharge/Weapon
 onready var weapon_charge: ProgressBar = $ColorRect/MarginContainer/HBoxContainer/Info/WeaponCharge/WeaponCharge
 
-
-func set_weapon_scale():
-	weapon_texture.rect_scale = Vector2(4.0, 4.0)
-	weapon_texture.rect_rotation = -25.0
-
 func set_lives_counter(lives: int):
 	lives_counter.text = String(lives)
 
@@ -26,6 +21,10 @@ func set_map(map: Map):
 
 func set_weapon_texture(texture: Texture):
 	weapon_texture.texture = texture
+	# Let the weapon texture rect update it's sprite first
+	yield(get_tree().create_timer(0.01),"timeout")
+	weapon_texture.rect_scale = Vector2(4.0, 4.0)
+	weapon_texture.rect_rotation = -25.0
 
 func set_weapon_charge(amount: float):
 	weapon_charge.value = amount
@@ -34,7 +33,6 @@ func glitch():
 	glitch_texture.material.set_shader_param("fade",0.01)
 	yield(get_tree().create_timer(0.1),"timeout")
 	glitch_texture.material.set_shader_param("fade",0.00)	
-	set_weapon_scale()
 
 func _on_pause_button_pressed():
 	get_tree().paused = true
