68b7849e2fd4517309a787ccf84ab0ca9044d063
bad <badatnames@tutanota.com>
2022-03-06 15:54:34 +0100

    Add timescale command

--- Autoloads/Init.gd
+++ Autoloads/Init.gd
@@ -6,3 +6,10 @@ func _ready():
 	seed(rng_seed)
 	# Log the seed for debugging
 	print("RNG seed:", rng_seed)
+
+	Console.add_command("timescale", self, "command_timescale")\
+	.add_argument("scale", TYPE_REAL)\
+	.register()
+
+func command_timescale(scale: float):
+	Engine.time_scale = scale
